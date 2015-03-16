ae37fb0404d792b4e323a114f39b514705702bdc
Aizaz AZ <its-me-az@outlook.com>
2015-03-16 12:28:58 +0800

    Update PhotoPreviewActivity.java

--- src/com/photoselector/ui/PhotoPreviewActivity.java
+++ src/com/photoselector/ui/PhotoPreviewActivity.java
@@ -1,5 +1,9 @@
 package com.photoselector.ui;
-
+/**
+ * 
+ * @author Aizaz AZ
+ *
+ */
 import java.util.List;
 
 import android.os.Bundle;
@@ -27,13 +31,13 @@ public class PhotoPreviewActivity extends BasePhotoPreviewActivity implements On
 		if (extras == null)
 			return;
 
-		if (extras.containsKey("photos")) { // 预览图片
+		if (extras.containsKey("photos")) { // 棰勮鍥剧墖
 			photos = (List<PhotoModel>) extras.getSerializable("photos");
 			current = extras.getInt("position", 0);
 			updatePercent();
 			bindData();
-		} else if (extras.containsKey("album")) { // 点击图片查看
-			String albumName = extras.getString("album"); // 相册
+		} else if (extras.containsKey("album")) { // 鐐瑰嚮鍥剧墖鏌ョ湅
+			String albumName = extras.getString("album"); // 鐩稿唽
 			this.current = extras.getInt("position");
 			if (!CommonUtils.isNull(albumName) && albumName.equals(PhotoSelectorActivity.RECCENT_PHOTO)) {
 				photoSelectorDomain.getReccent(this);
@@ -47,7 +51,7 @@ public class PhotoPreviewActivity extends BasePhotoPreviewActivity implements On
 	public void onPhotoLoaded(List<PhotoModel> photos) {
 		this.photos = photos;
 		updatePercent();
-		bindData(); // 更新界面
+		bindData(); // 鏇存柊鐣岄潰
 	}
 
 }
